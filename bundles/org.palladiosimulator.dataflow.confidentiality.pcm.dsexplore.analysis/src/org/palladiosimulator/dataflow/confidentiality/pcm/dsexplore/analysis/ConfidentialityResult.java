121f4e5b7612b23abfd4e4df9ce546b12720821c
Maximilian Walter <maximilian.walter@kit.edu>
2022-02-04 10:46:59 +0100

    changed confidentiality represntation

--- bundles/org.palladiosimulator.dataflow.confidentiality.pcm.dsexplore.analysis/src/org/palladiosimulator/dataflow/confidentiality/pcm/dsexplore/analysis/ConfidentialityResult.java
+++ bundles/org.palladiosimulator.dataflow.confidentiality.pcm.dsexplore.analysis/src/org/palladiosimulator/dataflow/confidentiality/pcm/dsexplore/analysis/ConfidentialityResult.java
@@ -15,9 +15,9 @@ public class ConfidentialityResult implements IAnalysisResult {
     @Override
     public double getValueFor(final Criterion criterion) {
         if (this.solution) {
-            return -1.0;
+            return 1.0;
         }
-        return 1.0;
+        return -1.0;
     }
 
 }
